ded14b0004d40112bc618ee140079da4c86d2eec
ajz34 <ajz34@outlook.com>
2024-12-07 11:17:54 +0800

    change build.rs to accept LD_LIBRARY_PATH finding of libcint.a

--- build.rs
+++ build.rs
@@ -1,3 +1,42 @@
+use std::{error::Error, path::PathBuf};
+
+/// Generate link search paths from a list of paths.
+///
+/// This allows paths like `/path/to/lib1:/path/to/lib2` to be split into individual paths.
+fn generate_link_search_paths(paths: &[Result<String, impl Error + Clone>]) -> Vec<String> {
+    paths
+        .iter()
+        .map(|path| {
+            path.clone()
+                .unwrap_or_default()
+                .split(":")
+                .map(|path| path.to_string())
+                .collect::<Vec<_>>()
+        })
+        .into_iter()
+        .flatten()
+        .filter(|path| !path.is_empty())
+        .collect::<Vec<_>>()
+}
+
+/// Check if the library is found in the given paths.
+fn check_library_found(
+    lib_name: &str,
+    lib_paths: &[String],
+    lib_extension: &[String],
+) -> Option<String> {
+    for path in lib_paths {
+        for ext in lib_extension {
+            let lib_path = PathBuf::from(&path).join(format!("lib{}.{}", lib_name, ext));
+            if lib_path.exists() {
+                return Some(lib_path.to_string_lossy().to_string());
+            }
+        }
+    }
+    return None;
+}
+
+
 fn main() {
     build_cint();
     build_ecp();
@@ -5,8 +44,23 @@ fn main() {
 
 /// Builds the `libcint` library from the provided path.
 fn build_cint() {
-    let dst = cmake::Config::new("external_deps").build();
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // search dirs
+    for key in ["CINT_DIR", "REST_EXT_DIR"].iter() {
+        println!("cargo:rerun-if-env-changed={}", key);
+    }
+    let lib_paths = generate_link_search_paths(&[
+        std::env::var("CINT_DIR"),
+        std::env::var("REST_EXT_DIR"),
+        std::env::var("LD_LIBRARY_PATH"),
+    ]);
+    if let Some(path) = check_library_found("cint", &lib_paths, &["a".to_string()]) {
+        let path = std::fs::canonicalize(path).unwrap();
+        let path = path.parent().unwrap().display();
+        println!("cargo:rustc-link-search=native={}", path);
+    } else {
+        let dst = cmake::Config::new("external_deps").build();
+        println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    }
     println!("cargo:rustc-link-lib=cint");
     println!("cargo:rustc-link-lib=quadmath");
 }
